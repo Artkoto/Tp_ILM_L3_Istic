d2f47954b30b08f6e66898fea5ae7e7e0da91945
Artkoto <arnaudakoto@live.fr>
2020-02-11 13:12:31 +0100

    maj

--- TP3/TP3/main.s
+++ TP3/TP3/main.s
@@ -7,58 +7,57 @@
 
 			.text
 
-			####################################
+			#####################################################
 			# Fonction rechercheDichotomique() 
+			#---------------------------------------------------
 			# r8:pos
-			# r9: fin-debut
-			# r10: fin-debut /2
-			# r11:4*pos
-			#r12:tableau +4*pos
-			# r13:tabl[pos]
-			####################################
+			# r9:4*pos : position de l'element  dans le tableau
+			#r10:@tabl[pos]
+			# r11:tabl[pos]
+			####################################################
 rechercheDichotomique:
 			addi	r2, zero, -1
-		
 
 conditionRech: 
-			bge r6 , r7, nonConditionRech
-			ret
+			bge r7 , r6, nonConditionRech 
+			#si debut > fin on retourne -1
+			ret 
 
 nonConditionRech:
+				#calcule de pos dans r8
 				mov r8 ,r7
 				sub r8 , r8 , r6
 				srli r8, r8, 1
 				add r8 , r8 ,r6
-
-				slli	r9, r8, 2 #position de t[i]
+				
+				#recuperation du pos iem element du tableau
+				slli	r9, r8, 2 #calcule de l'indice pos dans le tableaux
 				add		r10, r5, r9 #indice du tableau
-				ldw		r11, 0(r10)
-				subi sp , sp , 4
-				stw r11 , 0(sp)
+				ldw		r11, 0(r10) #recuperation de t[pos]
 
 conditionInbrRech1:
 				
 				bne		r11, r4, conditionInbrRech2
-				mov		r2, r8
-				ldw	r11, 0(sp)
-			    addi sp, sp, 4
+				mov		r2, r8 # si tab[pos] = val on retourn pos
 				ret
 
 conditionInbrRech2:
-				blt		r4, r11 , conditionInbrRech3
-				addi	r6, r8, 1
-				subi	sp, sp, 4
+				# on sauvegarde l'adresse du processus en cours dans la pile
+				#pour s'assurer que la recursivité se passe correctement
+				subi	sp, sp, 4 
 				stw		ra, 0(sp)
-				call	rechercheDichotomique
-				ldw		ra, 0(sp)
+
+				blt		r4, r11 , conditionInbrRech3
+				#si tab[pos] < val 
+				addi	r6, r8, 1 # debut := post + 1
+				call	rechercheDichotomique 
+				ldw		ra, 0(sp) 
 				addi	sp, sp, 4
 				ret
 
 
  conditionInbrRech3:
-				subi	r7, r8, 1
-				subi	sp, sp, 4
-				stw		ra, 0(sp)
+				subi	r7, r8, 1 # fin := post - 1
 				call	rechercheDichotomique
 				ldw		ra, 0(sp)
 				addi	sp, sp, 4
@@ -66,9 +65,15 @@ conditionInbrRech2:
 				
 				ret	
 
-			##############################
-			#       Fonction main()      #
-			##############################
+			################################
+			#       Fonction main()  
+			#-------------------------------
+			#          r4:val
+			#		  r5:tableau
+			#		  r6: debut (0)
+			#		  r7:fin (99)
+		    #         r8:pos
+			################################
 			.globl main
 main:		
 boucle:
@@ -80,48 +85,32 @@ boucle:
 			# Lit un nombre
 			addi	r2, zero, READ_INT
 			trap
-
-			##############################
-			# 		  le MAin :
-			#         r4:val
-			#		  r5:tableau
-			#		  r6: debut (0)
-			#		  r7:fin (99)
-		    #         r9:pos
-			##############################
+     
+			###############Complétion du Main############
 			mov r4,r2
 			movia r5,tableau
 			movia r6,0
-			movia r7,9
+			movia r7,99
 
 			subi sp, sp, 4
 			stw ra, 0(sp)
 			subi sp, sp, 4
 			stw r8,0(sp)
-			subi sp, sp, 4
-			stw r9,0(sp)
 
 
 			##############################
 			# 	Appelle de la fonction 
 			##############################
 
-			call rechercheDichotomique
-
-			ldw	r9, 0(sp)
-			addi sp, sp, 4
-			ldw	r8, 0(sp)
-			addi sp, sp, 4
-			ldw	ra, 0(sp)
-			addi sp, sp, 4
-			
+			call rechercheDichotomique	
 			mov		r8, r2
 
 conditionPos: 
-			bge r0, r8 , nonConditionPos
+			bge r8, r0 , nonConditionPos
 			movia r4 , msgErreur
 			movia r2, PRINT_STRING
 			trap
+			br	boucle
 
 nonConditionPos:
 			movia r4 , msgPos
@@ -130,6 +119,11 @@ nonConditionPos:
 			mov r4 , r8
 			movia r2, PRINT_INT
 			trap
+
+			ldw	r8, 0(sp)
+			addi sp, sp, 4
+			ldw	ra, 0(sp)
+			addi sp, sp, 4
 
 			br	boucle
 
